4089af2e0a44df73969295fc8c18fd3dcb02abc2
221RDB222 <124578710+MartinsBoikovs@users.noreply.github.com>
2023-02-13 14:49:24 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -41,8 +41,8 @@ def find_mismatch(text):
 
 def main():
     text = input()
-    if text[0:5] == 'I\\r\\n':
-        text = text[5:]
+    if text == 'I\r':
+        text = input()
     elif text == 'F':
         file = "./Datu struktūras/test1.txt"
         with open(file) as f:
