9b2e8661287d11fa4208b6b350f0cbf3fba6e037
221RDB222 <124578710+MartinsBoikovs@users.noreply.github.com>
2023-02-13 14:57:27 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -19,7 +19,8 @@ def find_mismatch(text):
             # Process opening bracket, write your code here
             opening_brackets_stack.append(Bracket(next, i))
             #Bracket.char = next
-            #Bracket.position = i+1
+            #Bracket.position = i
+            pass
             
 
         if next in ")]}":
@@ -29,19 +30,18 @@ def find_mismatch(text):
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[len(opening_brackets_stack)-1][0], next):
                 #print(i+1)
                 return i + 1 
-            #else :
+                
             if are_matching(opening_brackets_stack[len(opening_brackets_stack)-1][0], next):
                 opening_brackets_stack.pop()
             # Process closing bracket, write your code here
+            pass
     
     if opening_brackets_stack :
         return opening_brackets_stack[len(opening_brackets_stack)-1][1] + 1
 
-
-
 def main():
     text = input()
-    if text == 'I\r\n':
+    if 'I' in text:
         text = input()
     elif text == 'F':
         file = "./Datu struktūras/test1.txt"
